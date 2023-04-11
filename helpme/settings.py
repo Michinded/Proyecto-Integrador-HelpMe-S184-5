6c6bbfc229b4776f2270a6178ea466cfdaccbc86
Michinded <michinded@gmail.com>
2023-04-10 23:06:10 -0600

    Quit Secretkey

--- helpme/settings.py
+++ helpme/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_)ehz6ibo8#5r2j)ljttcwzw%lb-i(m&a%rud)va7k$fmpi(si'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
