06043c6c9986f9ae175b825d9f7a4b5e81a969df
Michinded <michinded@gmail.com>
2023-04-15 23:01:06 -0600

    Migrations Full
    Se agregaron todas las migraciones

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Pubs
+from .models import Imagenes
+from .models import Profiles
 from .models import Carrera
 from .models import Usuario
 
--- blogs/models.py
+++ blogs/models.py
@@ -25,6 +25,7 @@ class Usuario(models.Model):
 
 
 
+
 class Pubs(models.Model):
     name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, default='anonymous')
@@ -36,4 +37,53 @@ class Pubs(models.Model):
             self.url_img = self.picture.url
         super().save(*args, **kwargs)
 
+class Imagenes(models.Model):
+    name = models.CharField(max_length=50)
+    picture = models.ImageField(upload_to='profile_pics/')
+    picture_url = models.CharField(max_length=255, null=True, blank=True)
 
+    def save(self, *args, **kwargs):
+        if self.picture:
+            # Obtener la extensión del archivo
+            file_extension = self.picture.content_type.split('/')[-1]
+            # Renombrar el archivo antes de subirlo a S3
+            filename = f"{self.name}-profile-photo.{file_extension}"
+            self.picture.name = filename
+
+         # Subir el archivo a S3 y guardar la URL en el campo picture_url
+            storage = self.picture.storage
+            url = storage.save(self.picture.name, self.picture)
+            self.picture_url = storage.url(url)
+
+        super().save(*args, **kwargs)
+
+
+class Profiles(models.Model):
+    name = models.CharField(max_length=50)
+    picture = models.ImageField(upload_to='profile_pics/')
+    picture_url = models.CharField(max_length=255, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.picture:
+            # Renombrar el archivo antes de subirlo a S3
+            filename = f"{self.name}-profile-photo.png"
+            self.picture.name = filename
+
+            # Obtener el tipo de archivo a partir de la extensión
+            extension = self.picture.name.split('.')[-1]
+            if extension in ['png', 'jpg', 'jpeg', 'gif']:
+                content_type = f"image/{extension}"
+            else:
+                content_type = None
+        
+            # Subir el archivo a S3 y guardar la URL en el campo picture_url
+            storage = self.picture.storage
+            url = storage.save(self.picture.name, self.picture)
+            self.picture_url = storage.url(url)
+        
+            # Asignar el tipo de archivo al objeto
+            self.picture.content_type = content_type
+
+        super().save(*args, **kwargs)
+
+        super().save(*args, **kwargs)
