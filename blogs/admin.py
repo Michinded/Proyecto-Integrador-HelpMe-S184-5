7aa7b8083613a4074c83afd4fc86adb4505af325
Michinded <michinded@gmail.com>
2023-04-15 22:56:47 -0600

    Corrección de Migraciones

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from .models import Pubs
-from .models import Imagenes
-from .models import Profiles
 from .models import Carrera
 from .models import Usuario
 
 # Register your models here.
 admin.site.register(Pubs)
-admin.site.register(Imagenes)
-admin.site.register(Profiles)
 admin.site.register(Carrera)
 admin.site.register(Usuario)
